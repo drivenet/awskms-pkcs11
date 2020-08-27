61b058c6ad95d90d5064bd33a7dbdf41c3771d4d
Aristarkh Zagorodnikov <xm@drive.net>
2020-08-27 16:11:13 +0300

    Improve TokenManager logging

--- AwsKmsPkcs11/Service/TokenManager.cs
+++ AwsKmsPkcs11/Service/TokenManager.cs
@@ -2,13 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 
-using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 
 using Net.Pkcs11Interop.Common;
 using Net.Pkcs11Interop.HighLevelAPI;
-
-using static System.FormattableString;
 
 namespace AwsKmsPkcs11.Service
 {
@@ -18,14 +16,16 @@ namespace AwsKmsPkcs11.Service
 
         private readonly object _lock = new object();
         private readonly IOptionsMonitor<TokenOptions> _options;
+        private readonly ILogger _logger;
         private IPkcs11Library? _library;
 
-        public TokenManager(IOptionsMonitor<TokenOptions> options)
+        public TokenManager(IOptionsMonitor<TokenOptions> options, ILogger<TokenManager> logger)
         {
             _options = options ?? throw new ArgumentNullException(nameof(options));
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
         }
 
-        private IPkcs11Library Library
+        private IPkcs11Library? Library
         {
             get
             {
@@ -39,12 +39,11 @@ namespace AwsKmsPkcs11.Service
                         if (library is null)
                         {
                             _library = library = Factories.Pkcs11LibraryFactory.LoadPkcs11Library(Factories, libraryPath, AppType.MultiThreaded);
+                            if (library is null)
+                            {
+                                _logger.LogError(EventIds.LibraryLoadFailed, "Failed to load PKCS#11 library \"{LibraryPath}\".", libraryPath);
+                            }
                         }
-                    }
-
-                    if (library is null)
-                    {
-                        throw new InvalidOperationException(Invariant($"Failed to load PKCS#11 library \"{libraryPath}\"."));
                     }
                 }
 
@@ -61,7 +60,7 @@ namespace AwsKmsPkcs11.Service
 
         public byte[]? Encrypt(KeyDescription key, byte[] plaintext)
         {
-            var slot = SelectSlot(key);
+            var slot = SelectSlot(key.TokenSerialNumber);
             if (slot is null)
             {
                 return null;
@@ -72,7 +71,7 @@ namespace AwsKmsPkcs11.Service
 
         public byte[]? TryDecrypt(KeyDescription key, byte[] ciphertext)
         {
-            var slot = SelectSlot(key);
+            var slot = SelectSlot(key.TokenSerialNumber);
             if (slot is null)
             {
                 return null;
@@ -81,7 +80,7 @@ namespace AwsKmsPkcs11.Service
             return Decrypt(key.KeyId, slot, ciphertext);
         }
 
-        private static byte[]? Encrypt(byte keyId, ISlot slot, byte[] plaintext)
+        private byte[]? Encrypt(byte keyId, ISlot slot, byte[] plaintext)
         {
             using var session = slot.OpenSession(SessionType.ReadOnly);
             var query = new List<IObjectAttribute>
@@ -93,10 +92,12 @@ namespace AwsKmsPkcs11.Service
             var key = session.FindAllObjects(query).SingleOrDefault();
             if (key is null)
             {
+                _logger.LogError(EventIds.NoMatchingKey, "No public key with id {KeyId} on token with serial number \"{SerialNumber}\".", keyId, slot.GetTokenInfo().SerialNumber);
                 return null;
             }
 
             using var mechanism = session.Factories.MechanismFactory.Create(CKM.CKM_RSA_PKCS);
+            _logger.LogWarning(EventIds.Encrypt, "Encrypting with mechanism type {Mechanism} using key with id {KeyId} on token with serial number \"{SerialNumber}\".", mechanism.Type, keyId, slot.GetTokenInfo().SerialNumber);
             return session.Encrypt(mechanism, key, plaintext);
         }
 
@@ -110,7 +111,7 @@ namespace AwsKmsPkcs11.Service
             }
             catch (Pkcs11Exception exception) when (exception.RV == CKR.CKR_PIN_INCORRECT)
             {
-                //Incorrect PIN
+                _logger.LogError(EventIds.InvalidPin, "Invalid PIN for token with serial number \"{SerialNumber}\".", keyId, slot.GetTokenInfo().SerialNumber);
                 return null;
             }
 
@@ -125,10 +126,12 @@ namespace AwsKmsPkcs11.Service
                 var key = session.FindAllObjects(query).SingleOrDefault();
                 if (key is null)
                 {
+                    _logger.LogError(EventIds.NoMatchingKey, "No private key with id {KeyId} on token with serial number \"{SerialNumber}\".", keyId, slot.GetTokenInfo().SerialNumber);
                     return null;
                 }
 
                 using var mechanism = session.Factories.MechanismFactory.Create(CKM.CKM_RSA_PKCS);
+                _logger.LogWarning(EventIds.Decrypt, "Decrypting with mechanism type {Mechanism} using key with id {KeyId} on token with serial number \"{SerialNumber}\".", mechanism.Type, keyId, slot.GetTokenInfo().SerialNumber);
                 return session.Decrypt(mechanism, key, ciphertext);
             }
             finally
@@ -137,19 +140,35 @@ namespace AwsKmsPkcs11.Service
             }
         }
 
-        private ISlot? SelectSlot(KeyDescription key)
+        private ISlot? SelectSlot(string serialNumber)
         {
-            var slots = Library.GetSlotList(SlotsType.WithTokenPresent);
+            var slots = Library?.GetSlotList(SlotsType.WithTokenPresent);
+            if (slots is null)
+            {
+                return null;
+            }
+
             foreach (var slot in slots)
             {
                 var token = slot.GetTokenInfo();
-                if (token.SerialNumber == key.TokenSerialNumber)
+                if (token.SerialNumber == serialNumber)
                 {
                     return slot;
                 }
             }
 
+            _logger.LogWarning(EventIds.NoMatchingToken, "No token matching serial number \"{SerialNumber}\".", serialNumber);
             return null;
+        }
+
+        private static class EventIds
+        {
+            public static readonly EventId LibraryLoadFailed = new EventId(1, nameof(LibraryLoadFailed));
+            public static readonly EventId NoMatchingToken = new EventId(2, nameof(NoMatchingToken));
+            public static readonly EventId NoMatchingKey = new EventId(3, nameof(NoMatchingKey));
+            public static readonly EventId InvalidPin = new EventId(4, nameof(InvalidPin));
+            public static readonly EventId Encrypt = new EventId(5, nameof(Encrypt));
+            public static readonly EventId Decrypt = new EventId(6, nameof(Decrypt));
         }
     }
 }
