536f43f77aa1b1f4905cb24a7d9719c9b22fc256
Aristarkh Zagorodnikov <xm@x-infinity.com>
2025-05-05 15:48:51 +0100

    build: refresh project-system

--- publish-linux-x64.cmd
+++ publish-linux-x64.cmd
@@ -2,4 +2,3 @@
 rmdir /s /q packages\linux-x64\awskms-pkcs11
 mkdir packages\linux-x64\awskms-pkcs11
 dotnet publish AwsKmsPkcs11 --force --output packages\linux-x64\awskms-pkcs11 -c Integration -r linux-x64 --no-self-contained
-del packages\linux-x64\awskms-pkcs11\*.deps.json
